5837feed4c9c75f40cad78fa51a7d394b168701e
DESKTOP-O89J7H5\nepug <test>
2018-09-02 15:27:08 +0900

    必要なモデル一覧をアセットStoreからダウンロード

---
+++ Dungeon_Master/Assets/Script/UnityEventTap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class UnityEventTap : MonoBehaviour {
+
+    public UnityEvent OnTap;
+	
+
+	private void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (OnTap != null)
+            {
+                OnTap.Invoke();
+            }
+        }
+	}
+
+    //private bool OnTouchDown()
+    //{
+    //    if (0 < Input.touchCount)
+    //    {
+
+    //    }
+    //}
+}
